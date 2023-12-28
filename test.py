3115c3c4a8af3ec41e834236a6a4dbcd1d4c7b21
Jannik Reinhard <Jannik.Reinhard@outlook.de>
2023-12-28 11:11:36 +0100

    updates

--- test.py
+++
@@ -1,7 +0,0 @@
-from azure.identity import InteractiveBrowserCredential
-from msgraph import GraphServiceClient
-
-
-client = GraphServiceClient(credentials=InteractiveBrowserCredential(), scopes=['https://graph.microsoft.com/.default'])
-
-client.users
